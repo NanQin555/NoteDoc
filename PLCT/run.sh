94b6e30c2541f0c0c916d6dc7aad2297c7176a8a
NanQin555 <3135247064@qq.com>
2024-12-28 19:04:36 +0800

    update: warpAffine

--- PLCT/run.sh
+++ PLCT/run.sh
@@ -1,10 +1,15 @@
 USER="spacemit"
 REMOTE_IP="211.83.110.5"
+
 REMOTE_PATH="/home/spacemit/opencv"
 OPENCV_PATH="/home/nanqin/project/opencv"
 LOCAL_PATH="/home/nanqin/project/opencv/build/bin"
 PLCT="/home/nanqin/NoteDoc/PLCT"
 summary="/home/nanqin/project/opencv/modules/ts/misc/summary.py"
+
+TEST="imgproc"
+TEST_FILTER="--gtest_filter=\"*WarpAffine*\""
+PERF_FILTER="--gtest_filter=\"*WarpAffine*\""
 
 build=false
 upload=false
@@ -36,18 +41,18 @@ shift $((OPTIND -1))
 if [ "$build" = true ]; then
     cd ${OPENCV_PATH}/build
     ninja -j $(nproc)
-fi
+fi 
 
 if [ "$upload" = true ]; then
-    scp ${LOCAL_PATH}/opencv_test_core ${LOCAL_PATH}/opencv_perf_core ${USER}@${REMOTE_IP}:${REMOTE_PATH}
+    scp ${LOCAL_PATH}/opencv_test_core ${LOCAL_PATH}/opencv_perf_${TEST} ${USER}@${REMOTE_IP}:${REMOTE_PATH}
 fi
 
 if [ "$run_test" = true ]; then
-    ssh ${USER}@${REMOTE_IP} "${REMOTE_PATH}/opencv_test_core --gtest_filter=\"*MeanStdDev*\" " 
+    ssh ${USER}@${REMOTE_IP} "${REMOTE_PATH}/opencv_test_${TEST} ${TEST_FILTER}" 
 fi
 
 if [ "$run_perf" = true ]; then
-    ssh ${USER}@${REMOTE_IP} "${REMOTE_PATH}/opencv_perf_core --gtest_filter=\"*meanStdDev*\" --gtest_output=xml:${REMOTE_PATH}/result.xml"
+    ssh ${USER}@${REMOTE_IP} "${REMOTE_PATH}/opencv_perf_${TEST} ${PERF_FILTER} --gtest_output=xml:${REMOTE_PATH}/result.xml"
     scp ${USER}@${REMOTE_IP}:${REMOTE_PATH}/result.xml ${PLCT}/xml
     ${summary} ${PLCT}/xml/original.xml ${PLCT}/xml/result.xml
 fi
--- PLCT/wrapAffine.md
+++ PLCT/wrapAffine.md
@@ -1,16 +1,38 @@
+# opencv
+
+vscode + riscv64-unknown-linux-gnu-gdb 调试: 先手动启动qemu
+
+cmake -G Ninja .. -DCMAKE_TOOLCHAIN_FILE=../platforms/linux/riscv64-gcc.toolchain.cmake -DBUILD_EXAMPLES=OFF -DWITH_OPENCL=OFF  -DTOOLCHAIN_COMPILER_LOCATION_HINT=/opt/riscv/bin -DENABLE_RVV=ON -DWITH_HAL_RVV=ON -DBUILD_SHARED_LIBS=OFF -DOPENCV_EXTRA_CXX_FLAGS="-static -static-libgcc -static-libstdc++" -DOPENCV_EXTRA_C_FLAGS="-static -static-libgcc -static-libstdc++" -DCMAKE_BUILD_TYPE=Debug
+
+测试: 
+1. " ./opencv_test_imgproc --gtest_filter="*WarpAffine*" "
+2. " ./opencv_perf_imgproc --gtest_filter="*WarpAffine*" --gtest_output=xml:./result.xml"
+   
 # 仿射变换
 
 二维坐标的线性变换
 
 图像翻转(Flip)，旋转(Rotations)，平移(Translations)，缩放(Scale)等
 
+## 插值
+
+插值: 是一种估计或计算两个已知数据点之间未知数据点的技术。在图像处理中，插值用于估计在变换过程中像素位置之间的像素值。
+
 # 目标函数
+
+                    warpAffine
+                        |
+                WarpAffineInvoker
+               /                 \
+warpAffineBlocklineNN             warpAffineBlockline
 
 ## warpAffine
 
 处理整个图的仿射变换
 
-M[6]
+矩阵 dst = 矩阵 src * M[6]
+
+M[6]: 通过不同的值代表不同的变换
 ```
 x' = x*M[0] + y*M[1] + M[2];
 y' = x*M[3] + y*M[4] + M[5];
@@ -27,3 +49,11 @@ y' = x*M[3] + y*M[4] + M[5];
 ## warpAffineBlockline
 
 处理单个块行的仿射变换，使用更复杂的插值方法
+
+## WarpAffineInvoker->operator
+
+1. 分块处理
+2. 计算仿射变换
+3. 插值处理
+4. 并行化处理
+
